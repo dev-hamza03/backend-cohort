1a15da10b403a495e2a104e6166b235988e3ee00
Hamza <devhamza03@gmail.com>
2026-01-23 20:47:22 +0530

    first commit

---
+++ day-1/app.js
@@ -0,0 +1,11 @@
+// const catMe = require('cat-me');
+
+// console.log(catMe())    this is for cat-me package to run
+
+
+
+const express = require("express");
+
+const app = express();    /// created the server
+
+app.listen(3000)       // .lesten() start the server
---
+++ day-2/app.js
@@ -0,0 +1,18 @@
+
+const express = require("express");
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("Hello world")
+})
+
+app.get('/home', (req, res) => {
+  res.send("This is  page")
+})
+
+app.get('/about', (req, res) => {
+  res.send("This is about page")
+})
+
+app.listen(3000);
