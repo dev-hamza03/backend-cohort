cde6d44dadd9457723f2761c6a4bd668b3d90741
Hamza <dev.hamza03@gmail.com>
2026-02-06 20:43:30 +0530

    day-12 created user in database and generate JWT_TOKEN and store token in cookies

--- day-12/backend/server.js
+++ day-12/backend/server.js
@@ -1,5 +1,7 @@
 const app = require("./src/app");
+const connectToDb = require("./src/config/database");
 
+connectToDb();
 
 app.listen(3000, () => {
     console.log("server is running on port 3000");
