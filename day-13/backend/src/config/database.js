b5e3deacc142e10e76ab6eb0c920452adb0a064d
Hamza <dev.hamza03@gmail.com>
2026-02-09 12:46:04 +0530

    day-13 connected to db and create user model and server is running

---
+++ day-13/backend/src/config/database.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+function connectToDb () {
+    mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log("Connected to database");
+    })
+}
+
+module.exports = connectToDb;
