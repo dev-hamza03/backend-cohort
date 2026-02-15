ff23e300221db9e251ff216ccdb0047bb60aadf1
Hamza <dev.hamza03@gmail.com>
2026-02-15 15:11:04 +0530

    created createPost API

--- day-14/backend/src/controllers/post.controller.js
+++ day-14/backend/src/controllers/post.controller.js
@@ -1,9 +1,20 @@
-const postModel = require("../models/post.model")
+const postModel = require("../models/post.model");
+const ImageKit = require("@imagekit/nodejs");
+const { toFile } = require("@imagekit/nodejs");
 
-
+const imagekit = new ImageKit({
+    privateKey: process.env.IMAGEKIT_PRIVATE_KEY
+});
 
 async function createPostController(req, res) {
     console.log(req.body, req.file);
+
+    const file = await imagekit.files.upload({
+        file: await toFile(Buffer.from(req.file.buffer), 'file'),
+        fileName: "test"
+    });
+
+    res.send(file);
 };
 
 
